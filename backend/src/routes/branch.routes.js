5bd4dae08da6080c67526a49168f591d89195c75
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-02-08 21:23:26 +0530

    branch curd operations fixed with strandard api

--- backend/src/routes/branch.routes.js
+++ backend/src/routes/branch.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addBranch, getAllBranches } from "../controllers/branch.controller.js";
+import { addBranch, getAllBranches, updateBranch, deleteBranch } from "../controllers/branch.controller.js";
 
 const router = Router();
 
 router.route("/").post(addBranch).get(getAllBranches);
+router.route("/:id").delete(deleteBranch).put(updateBranch);
 
 export default router;
