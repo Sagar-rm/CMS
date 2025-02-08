fb3c14ee15bcde128af5277e78e79c26d16e01be
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-02-08 21:33:01 +0530

    subject curd operations standardized

--- backend/src/routes/branch.routes.js
+++ backend/src/routes/branch.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addBranch, getAllBranches, updateBranch, deleteBranch } from "../controllers/branch.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/").post(addBranch).get(getAllBranches);
-router.route("/:id").delete(deleteBranch).put(updateBranch);
+router.route("/").post(verifyJWT, addBranch).get(getAllBranches);
+router.route("/:id").delete(verifyJWT, deleteBranch).put(verifyJWT, updateBranch);
 
 export default router;
